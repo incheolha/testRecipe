42c63892c2a350dd1c029849de8cea5415a1b41b
incheolha <incheolha@gmail.com>
2022-02-08 11:13:18 +0900

    ㅕㅔㅇㅁㅅㄷㅇ

--- newRecipte/src/app/shopping-list/shopping-list.service.ts
+++ newRecipte/src/app/shopping-list/shopping-list.service.ts
@@ -19,4 +19,13 @@ addIngredient(ingredient: Ingredient) {
   this.ingredients.push(ingredient);
   this.ingredientChanged.emit(this.ingredients.slice());
 }
+
+addIngredients(ingredients: Ingredient[]) {
+
+  // for( let ingredient of ingredients ) {
+  //   this.addIngredient(ingredient);
+  // }
+  this.ingredients.push(...ingredients);
+  this.ingredientChanged.emit(this.ingredients.slice());
+}
 }
